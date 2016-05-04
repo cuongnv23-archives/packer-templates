0dc8049589293e1524c5a9774b6562e084ea2421
Cuong Nguyen <cuongnguyen23@gmail.com>
2016-05-04 11:11:16 +0700

    Install EPEL repository

--- centos-7-x86_64/scripts/packages.sh
+++ centos-7-x86_64/scripts/packages.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
+
+# Install EPEL repository
+rpm -ivh \
+    http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-6.noarch.rpm
+
+# Install utilities
 yum install -y vim wget curl man tmux htop strace \
     rsync bind-utils openssl zip unzip sharutils psmisc
